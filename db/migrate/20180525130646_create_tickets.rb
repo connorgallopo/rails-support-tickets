d93f5a22d9f74257c1ddfd6ac28e582c88c4239f
Connor Gallopo <connor.gallopo@me.com>
2018-05-25 09:38:15 -0400

    added migrations

--- db/migrate/20180525130646_create_tickets.rb
+++ db/migrate/20180525130646_create_tickets.rb
@@ -1,7 +1,11 @@
 class CreateTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :tickets do |t|
-
+      t.string :title
+      t.string :body
+      t.string :status
+      t.integer :user_id
+      t.date :due_date
       t.timestamps
     end
   end
--- db/migrate/20180525130653_create_comments.rb
+++ db/migrate/20180525130653_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.string :content
+      t.integer :user_id
+      t.integer :ticket_id
       t.timestamps
     end
   end
