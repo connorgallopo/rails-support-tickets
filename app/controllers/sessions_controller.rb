040e2029f2eae7a41b721c36c3c7095b14ad3d09
Connor Gallopo <connor.gallopo@me.com>
2018-06-14 14:47:49 -0400

    worked on views for tickets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:email][:name])
+    if @user && @user.authenticate(params[:email][:password])
+      session[:user_id] = @user.id
+      redirect_to tickets_path, notice: "Thanks for signing in"
+    else
+      redirect_to signin_path
+    end
+  end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,8 @@ class TicketsController < ApplicationController
   def new
     @ticket = Ticket.new
   end
+
+  def index
+    @tickets = Ticket.all
+  end
 end
