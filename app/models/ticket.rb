3adf47cc9973a0405ebc4ec6ca20fb6342b2e887
Connor Gallopo <connor.gallopo@me.com>
2018-06-14 10:01:47 -0400

    added some styling and images, also worked out issues with routes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  def ticket_preview
+    body
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
   resources :users
   resources :tickets
 end
