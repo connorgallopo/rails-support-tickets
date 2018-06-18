a1afb6f35260d613e8d994f6ab4130b05d5ba9b5
Connor Gallopo <connor.gallopo@me.com>
2018-06-18 16:13:16 -0400

    added static home page and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def logged_in?
-    true if session[:user_id]
-  end
-
-  def current_user
-    User.find(session[:user_id])
-  end
-
-  def admin?
-    binding.pry
-    # current_user.admin == true
-  end
 end
