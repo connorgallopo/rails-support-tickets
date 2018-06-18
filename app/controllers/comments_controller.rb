4de66f911137b0729443170489b7f5027674df78
Connor Gallopo <connor.gallopo@me.com>
2018-06-18 12:57:44 -0400

    added view for comments and form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
 class CommentsController < ApplicationController
   def new
   end
+
+  def create
+    @comment = Comment.create(comment_params)
+    @comment.user = User.find(session[:user_id])
+    @comment.save
+    redirect_to ticket_path(@comment.ticket)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :ticket_id)
+  end
 end
