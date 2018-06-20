48b2702bf63e0b7197f36c449cd5e8ad63be07e1
Connor Gallopo <connor.gallopo@me.com>
2018-06-20 10:04:09 -0400

    added DRY errors for all forms

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :require_login
   before_action :require_admin
-  skip_before_action :verify_authenticity_token  
+  skip_before_action :verify_authenticity_token
   def index; end
 
   def promote
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def admin?
     current_user.admin == true
   end
+
+  def form_errors_for(object=nil)
+  render('shared/form_errors', object: object) unless object.blank?
+end
 end
