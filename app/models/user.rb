b0165f9f7e2114186d9e6db73be18257719ee30f
Connor Gallopo <connor.gallopo@me.com>
2018-07-16 19:08:09 -0400

    Added Devise and started to work on bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :tickets
   has_many :comments, through: :tickets
-
-  validates :email, uniqueness: true
-  validates :email, presence: true
 end
